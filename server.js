02bdf29199c74a7de940466c7e24477857a867f0
JustinYurasits <yurasits@gmail.com>
2022-10-06 15:47:12 -0400

    Lesson 13 - Creating Routes and Responses
    Added a first route to '/'
    Worked with a number of responses
    Set up routes for GET, POST, PUT, DELETE

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//This sets up a route for get requests to '/api/v1/bootcamps'
+app.get('/api/v1/bootcamps', (req, res) => {
+  //res.send({ name: 'Justin' }); //don't need json.stringify, Express will convert this to JSON
+  //res.sendResponse(400);//sends a response
+  //res.json({ name: 'Justin' }); // Specifiy JSON for the response
+  //res.status(400).json({ success: false }); //combine response with content to send
+  res.status(200).json({ success: true, data: { msg: 'Show all Bootcamps' } }); //combine response with content to send
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, data: { msg: `Get bootcamp ${req.params.id}` } });
+});
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, data: { msg: 'Create new Bootcamp' } });
+});
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: { msg: `Update bootcamp ${req.params.id}` },
+  });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, data: { msg: `Delete bootcamp ${req.params.id}` } });
+});
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(
