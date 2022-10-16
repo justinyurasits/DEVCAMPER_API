aec501eddd2a8395d614ab1966ed78240d0711c2
JustinYurasits <yurasits@gmail.com>
2022-10-16 08:10:08 -0400

    Lesson 28 - Mongoose Error Handling 2
    Add additional conditional error handlers to /middleware/error.js to check for duplicate and validation errors.

--- middleware/error.js
+++ middleware/error.js
@@ -2,15 +2,34 @@ const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
-  error.message = err.message; //wtf
+  //error.message = err.message; //wtf
 
   //Log to console for dev
-  console.log(err.stack.red);
+  //console.log(error);
+  //console.dir(err);
+  console.log(err);
+  console.log(err.name); //where does this come from!?!?!
+  //console.log(err.kind);
+  //console.log(err.stack);
 
   //Mongoose bad object ID
   if (err.name === 'CastError') {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered for the bootcamp';
+    error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    console.log('Justin');
+    console.log(err.errors);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
